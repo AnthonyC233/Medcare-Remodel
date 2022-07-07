df1f9ded21c2edcf94be68d96112cc0e647eff95
Lina Choi <choilina16@gmail.com>
2022-07-07 13:18:31 -0400

    redo the about section to make the media queries work

--- src/components/About (Home)/About.js
+++ src/components/About (Home)/About.js
@@ -1,52 +1,83 @@
 import React from 'react';
 import './about.css';
-// Linking the progress bar js file
+// LINK TO PROGRESS BARS
 import Progress from './Progress';
 
 function About() {
   return (
-    <section>
-      <div className="grid grid-cols-2 lg:grid-cols-1">
-        <div className="p-10 bg-ltGreen flex items-center justify-center flex-col text-left">
-          <div
-            className="about-section pl-24  
+    <section className="grid grid-cols-2 lg:grid-cols-1">
+      {/* ABOUT US SECTION */}
+      {/* 36: 144px | 12: 48px | Original pl-24: 96px*/}
+      <div
+        className="bg-ltGreen flex justify-center items-center flex-col
 
-            // WHEN NAV BAR GOES UP 
-            sm:p-0  
+        // BASE
+        pl-36 py-12 pr-12
 
-            // PHONES | LANDSCAPE
-            iPhoneSE:p-0
-            iPhoneXR:p-0
-            iPhone12Pro:p-0
-            Pixel5:p-0
-            SamsungS8:p-0
-            SamsungS20Ultra:p-0
-            GalaxyFoldH:p-0"
-          >
-            <h1 className="pb-5 text-3xl font-medium font-['Mingzat']">
-              ABOUT US
-            </h1>
-            <p>
-              At our school, we pride ourselves in gapping bridges for our
-              students to become future MDs, PAs and Nurses. Our school is open
-              to all college students majoring in any of the sciences.
-            </p>
-            <br></br>
-            <p>
-              We are a flexible, accelerated, and affordable certification
-              school for undergraduate and graduate science majors.
-            </p>
-          </div>
-        </div>
-        {/* Progress bar can be customized by changing the "done=" value */}
-        <div className="p-10 progress-bars bg-green flex items-center justify-center flex-col xxsm:hidden">
-          <p className="pb-2">AVERAGE CERTIFICATION COMPLETION</p>
-          <Progress done="80" />
-          <p className="pb-2">AVERAGE GRADUATES EMPLOYED</p>
-          <Progress done="80" />
-          <p className="pb-2">AVERAGE FUN</p>
-          <Progress done="100" />
-        </div>
+        // SMALL SCREEN
+        sm:pl-12
+
+        // PHONES | LANDSCAPE
+        SamsungS20Ultra:pl-12
+        SamsungS8:pl-12
+        Pixel5:pl-12
+        iPhone12Pro:pl-12
+        iPhoneXR:pl-12
+        iPhoneSE:pl-12 
+        GalaxyFold:pl-12
+        "
+      >
+        <h1 className="pb-5 text-3xl font-semibold font-['Mingzat']">
+          ABOUT US
+        </h1>
+        <p>
+          At our school, we pride ourselves in gapping bridges for our students
+          to become future MDs, PAs and Nurses. Our school is open to all
+          college students majoring in any of the sciences.
+        </p>
+        <br></br>
+        <p>
+          We are a flexible, accelerated, and affordable certification school
+          for undergraduate and graduate science majors.
+        </p>
+      </div>
+
+      {/* PROGRESS BARS SECTION */}
+      <div
+        className="progress-container bg-green flex justify-center items-center flex-col
+
+        // BASE
+        pl-12 py-12 pr-12
+
+        lg:pl-36
+
+        sm:pr-36
+
+        sm:hidden
+
+
+        // PHONES | LANDSCAPE
+        SamsungS20Ultra:pl-12
+        SamsungS8:pl-12
+        Pixel5:pl-12
+        iPhone12Pro:pl-12
+        iPhoneXR:pl-12
+        iPhoneSE:pl-12 
+        GalaxyFold:pl-12
+        "
+      >
+        <p className="text-xs font-semibold font-['Mingzat']">
+          AVERAGE CLASSROOM SIZE
+        </p>
+        <Progress done="50" />
+        <p className="pt-5 text-xs font-semibold font-['Mingzat']">
+          AVERAGE GRADUATES EMPLOYED
+        </p>
+        <Progress done="80" />
+        <p className="pt-5 text-xs font-semibold font-['Mingzat']">
+          AVERAGE FUN
+        </p>
+        <Progress done="100" />
       </div>
     </section>
   );
