8c916b745e47e25f39d7313d672d6c746eaab17c
AnthonyC233 <changanthony61@yahoo.com>
2022-07-13 12:58:47 -0400

    changes to pages (add title)

--- src/pages/Credits.js
+++ src/pages/Credits.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Nav from '../components/Header/Nav';
 import CreditsComp from '../components/Credits (Credits)/CreditsComp';
+import {Helmet} from "react-helmet";
+
 
 const Credits = () => {
   return (
     <div>
+        <>
+      <Helmet>
+        <title>Credit</title>  
+      </Helmet>
+      </>
       <Nav />
       <CreditsComp />
     </div>
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Nav from '../components/Header/Nav';
 import ErrorComp from '../components/Error (Error)/ErrorComp';
+import {Helmet} from "react-helmet";
+
 
 const Error = () => {
   return (
     <div>
+        <>
+      <Helmet>
+        <title>Error</title>  
+      </Helmet>
+      </>
       <Nav />
       <ErrorComp />
     </div>
